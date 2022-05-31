f8891bbe2750116db5a29c0322944e2e23679515
Edmilson Silva <56802572+DevEddi@users.noreply.github.com>
2022-05-31 14:30:42 -0300

    organizando os componentes

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import RouteStack from './routes/RouteStack';
 
+import RouteStack from './src/routes/RouteStack';
 
 export default function App() {
   return (
-    
     <RouteStack
     
     />
