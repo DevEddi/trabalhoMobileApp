20e8b8e404ab1e5d530c29b7df61af549975e8df
Edmilson Silva <scovinowork@gmail.com>
2022-10-22 17:41:14 -0300

    update login and sign up

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,18 +1,108 @@
-import { View } from 'react-native';
+import React, { useState} from 'react';
+import {
+    Text,
+    View,
+    TextInput,
+    TouchableOpacity,
+    Image,
+    ScrollView
+
+  } from 'react-native';
+
+import { KeyboardAvoidingView } from 'react-native';
+import { MotiView } from 'moti';
 
 import ScreensStyles from '../styles/screensStyles/ScreensStyles'
-import LoginForm from '../components/appEntry/login/Login';
+
 
 export default function Login({navigation}) {
+  const [email, setEmail] = useState(null)  
+  const [password, setPassword] = useState(null)
+
+  function acessUser(){       
+    if (email != null && password != null){
+        let emailUser = 'rick@gmail.com'
+        let passwordUser = 123456
+        if(email == emailUser && password == passwordUser){
+          props.sendForm()
+        }
+    }
+  }
   return (
-    <View style={ScreensStyles.containerLogin}>          
-        <View>
-          <LoginForm
-              sendForm = {() => navigation.navigate('Dashboard')}
-              nameTouch = 'Login'  
-          />  
-        </View>         
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={130}
+    >
+      <ScrollView>
+          <View style={ScreensStyles.container}>
+            <MotiView
+              from={{
+                scale: 0,
+                translateX: -10,
+                translateY: -20,
+              }}
+              animate={{
+                scale: 1,
+                translateX: 0,
+                translateY: 0,
+      
+              }}
+              transition={{
+                type: 'spring',
+                duration: 10000,
+              }}
+            
+            >
+              <Image
+                style={ScreensStyles.imageRick}
+                source={require('../images/logoRickNormal.png')}
+              /> 
+            </MotiView>
+          <Text style={ScreensStyles.text}>Login</Text>
+          <MotiView
+            from={{
+              opacity: 0,
+            }}
+            animate={{
+              opacity: 1,
+    
+            }}
+            transition={{
+              type: 'timing',
+              duration: 3000,
+            }}
+              
+          >
+            <Text style={ScreensStyles.textLabel}>E-mail:</Text>
+            <TextInput
+              style={ScreensStyles.input}
+              placeholder="ricksanches@mail.com"
+              keyboardType='email-address'    
+              value={email}
+              onChangeText={setEmail}     
+
+            />
+            <Text style={ScreensStyles.textLabel}>Password:</Text>
+            <TextInput
+              style={ScreensStyles.input}
+              placeholder="*******"
+              keyboardType='default'
+              secureTextEntry={true}
+              value={password}
+              onChangeText={setPassword}
+              
+            />
+            <Text style={ScreensStyles.textForgot}>Forgot password ?</Text>
+            <TouchableOpacity
+              style={ScreensStyles.buttonStyle}
+            >           
+              
+            </TouchableOpacity>
+          </MotiView>      
+        </View>
+      </ScrollView>      
+    </KeyboardAvoidingView>
   );
 }
+
 
--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,16 +1,98 @@
-import { View } from 'react-native'; 
+import React, { useState } from 'react';
+import {
+    Text,
+    View,
+    TextInput,
+    TouchableOpacity,
+    Image,
+    ScrollView,
+  } from 'react-native';
+
+import { KeyboardAvoidingView } from 'react-native';
+import { MotiView } from 'moti'
+
 
 import ScreensStyles from '../styles/screensStyles/ScreensStyles'
-import SignUpForm from '../components/appEntry/signUp/SignUp';
 
 export default function SignUp( { navigation } ) {
+  const [email, setEmail] = useState(null)
+  const [password, setPassword] = useState(null)
+
+  
   return (
-    <View style={ScreensStyles.containerSignUp}>        
-        <SignUpForm
-          sendForm = {() => navigation.navigate('Dashboard')}
-          nameTouch = 'Sign Up'          
-        />              
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={130}
+    >
+     <ScrollView>
+      <View  style={ScreensStyles.container}>
+        <MotiView
+            from={{
+              scale: 0,
+              translateX: -10,
+              translateY: -20,
+            }}
+            animate={{
+              scale: 1,
+              translateX: 0,
+              translateY: 0,
+    
+            }}
+            transition={{
+              type: 'spring',
+              duration: 10000,
+            }}          
+          >
+            <Image
+                style={ScreensStyles.imageRickCrazy}
+                source={require('../images/logoRickCrazy.png')}
+              /> 
+        </MotiView> 
+        
+        <MotiView
+          from={{
+            opacity: 0,
+          }}
+          animate={{
+            opacity: 1,
+  
+          }}
+          transition={{
+            type:'timing',
+            duration: 5000,
+          }}
+        >
+        <Text style={ScreensStyles.text}>Sign Up</Text>            
+        
+
+          <Text style={ScreensStyles.textLabel}>E-mail:</Text>
+          <TextInput
+            style={ScreensStyles.input}
+            placeholder="ricksanches@mail.com"
+            keyboardType='email-address'
+            value={email}
+            onChangeText={setEmail} 
+          />
+
+          <Text style={ScreensStyles.textLabel}>Password:</Text>
+          <TextInput
+            style={ScreensStyles.input}
+            placeholder="*****"
+            keyboardType='default'
+            secureTextEntry={true}   
+            value={password}
+            onChangeText={setPassword}      
+          />
+          <TouchableOpacity
+            style={ScreensStyles.buttonStyle}
+          >           
+            
+          </TouchableOpacity>   
+        </MotiView>   
+      </View>
+    </ScrollView> 
+  </KeyboardAvoidingView>
   );
 }
+
 
